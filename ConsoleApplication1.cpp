079c3279a8478af8088d835ddd3ee9036dcf4ea5
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-20 21:33:25 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A;
+	cin >> A;
+    while(A>0) // ^ 10^6 == 1001 | 110 = 1000 = 8
+	{
+	  cout << A % 10;
+	  A /= 10;
+	}
+
+
+}
